cd0bf7c2b053da0fee923c1d2429fbcea8c5ba85
Srinivas <srinivas365@proton.me>
2023-06-10 18:24:04 +0530

    Added mock backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

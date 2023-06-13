daad1039340d7689c12a4a06db290cc563e02914
Srinivas <srinivas365@proton.me>
2023-06-13 11:24:18 +0530

    added basic login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,2 +1,26 @@
 export function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+                <label>
+                    <span>Username</span>
+                    <input type="text" placeholder="Enter username" required />
+                </label>
+                <label>
+                    <span>Password</span>
+                    <input type="password" placeholder="Password" required />
+                </label>
+                <div>
+                    <label>
+                        <input type="checkbox" />
+                        <span>Remember me</span>
+                    </label>
+                    <a href="/">Forgot your password?</a>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <a href="/signup">Create New Account</a>
+        </div>
+    );
 }

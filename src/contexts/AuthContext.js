c40049aa48558475b4876dfd1357279690fb8801
Srinivas <srinivas365@proton.me>
2023-06-15 11:39:19 +0530

    feature: added login handler

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,7 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+    const [encodedToken, setEncodedToken] = useState();
+    const [user, setUser] = useState();
+
+    const loginHandler = async (userCredentials) => {
+        const response = await fetch("/api/auth/login", {
+            method: "POST",
+            body: JSON.stringify(userCredentials),
+        });
+
+        const { encodedToken, foundUser } = await response.json();
+
+        setEncodedToken(encodedToken);
+        setUser(foundUser);
+    };
+
+    return (
+        <AuthContext.Provider value={{ loginHandler }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,55 +1,70 @@
 import "./index.css";
 
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
+
 export function Login() {
+    const { loginHandler } = useContext(AuthContext);
     return (
-        <div className="login-container">
-            <div className="login">
-                <h2 className="login__title">Login</h2>
-                <form className="form">
-                    <div className="form__field">
-                        <label for="username" className="form__label">
-                            Username
-                        </label>
-                        <input
-                            name="username"
-                            type="text"
-                            required
-                            className="form__input"
-                        />
+        <div className="login">
+            <h2 className="login__title">Login</h2>
+            <form className="form">
+                <div className="form__field">
+                    <label for="username" className="form__label">
+                        Username
+                    </label>
+                    <input
+                        name="username"
+                        type="text"
+                        required
+                        className="form__input"
+                    />
+                </div>
+
+                <div className="form__field">
+                    <div className="form__label">
+                        <label for="password">Password</label>
+                        <a
+                            href="/"
+                            className="link link--decor-none link--primary-color"
+                        >
+                            Forgot your password?
+                        </a>
                     </div>
 
-                    <div className="form__field">
-                        <div className="form__label">
-                            <label for="password">Password</label>
-                            <a
-                                href="/"
-                                className="link link--decor-none link--primary-color"
-                            >
-                                Forgot your password?
-                            </a>
-                        </div>
+                    <input
+                        name="password"
+                        type="password"
+                        required
+                        className="form__input"
+                    />
+                </div>
+                <button type="submit" className="button button--primary">
+                    Login
+                </button>
+            </form>
+            <button
+                className="button button--text-only login__guest"
+                onClick={() => {
+                    const userCredentials = {
+                        username: "adarshbalika",
+                        password: "adarshBalika123",
+                    };
 
-                        <input
-                            name="password"
-                            type="password"
-                            required
-                            className="form__input"
-                        />
-                    </div>
-                    <button type="submit" className="button button--primary">
-                        Login
-                    </button>
-                </form>
-                <p className="new-account">
-                    <span className="new-account__text">Not a member? </span>
-                    <a
-                        href="/signup"
-                        className="new-account__link link link--decor-none link--primary-color"
-                    >
-                        Create New Account
-                    </a>
-                </p>
-            </div>
+                    loginHandler(userCredentials);
+                }}
+            >
+                Login as Guest
+            </button>
+            <p className="new-account">
+                <span className="new-account__text">Not a member? </span>
+                <a
+                    href="/signup"
+                    className="new-account__link link link--decor-none link--primary-color"
+                >
+                    Create New Account
+                </a>
+            </p>
         </div>
     );
 }

a97c5b50a282a07aee22e7e3b50e1583ee414a95
Srinivas <srinivas365@proton.me>
2023-06-16 21:53:04 +0530

    updated auth context to provide token and user data across app

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -24,7 +24,7 @@ export function AuthProvider({ children }) {
     };
 
     return (
-        <AuthContext.Provider value={{ loginHandler }}>
+        <AuthContext.Provider value={{ encodedToken, user, loginHandler }}>
             {children}
         </AuthContext.Provider>
     );

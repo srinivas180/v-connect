979f59103461230d474113f42daf7309a08eb07e
Srinivas <srinivas365@proton.me>
2023-06-16 21:27:23 +0530

    refactor: removed header and added route to home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
+import { Home } from "./pages/Home";
 import "./App.css";
-import { Header } from "./components/Header";
 
 function App() {
     return (
         <div className="App">
-            <Header />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
             </Routes>

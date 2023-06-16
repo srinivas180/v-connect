b1e6394ef9eba365cade9dca7631f622bc266db1
Srinivas <srinivas365@proton.me>
2023-06-16 21:38:19 +0530

    added basic nav in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import "./index.css";
 
@@ -10,6 +10,20 @@ export function Header() {
                     <h1 className="logo__name">V-Connect</h1>
                 </Link>
             </div>
+            <nav className="nav">
+                <NavLink className="nav__link link link--decor-none">
+                    Home
+                </NavLink>
+                <NavLink className="nav__link link link--decor-none">
+                    Explore
+                </NavLink>
+                <NavLink className="nav__link link link--decor-none">
+                    Bookmarks
+                </NavLink>
+                <NavLink className="nav__link link link--decor-none">
+                    Profile
+                </NavLink>
+            </nav>
         </header>
     );
 }

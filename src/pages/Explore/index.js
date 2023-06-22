d957a30cadc5e365ca0af9a0dc4571b24b49375f
Srinivas <srinivas365@proton.me>
2023-06-22 22:41:37 +0530

    added explore page

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,3 +1,34 @@
+import { useContext } from "react";
+
+import { Header } from "../../components/Header";
+import { Suggestions } from "../../components/Suggestions";
+import { Post } from "../../components/Post";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { PostsContext } from "../../contexts/PostsContext";
+
 export function Explore() {
-    return <h1>Explore</h1>;
+    const { loggedInUser } = useContext(AuthContext);
+    const { posts } = useContext(PostsContext);
+    return (
+        <div className="container">
+            <Header />
+            <div className="posts">
+                <h2 className="heading">Explore</h2>
+                {posts
+                    ?.filter(({ username }) => {
+                        // filter and then show logged in user and
+                        // following users posts
+                        return !(loggedInUser.username === username);
+                    })
+                    .map((post) => (
+                        <Post key={post._id} post={post} />
+                    ))}
+            </div>
+            <div className="search-and-suggestions">
+                <input type="search" placeholder="Search for users" />
+                <Suggestions />
+            </div>
+        </div>
+    );
 }
